b605aff81dbe703690e384e48fd626c6884c7f1b
Narow <fklasjd@freenet.de>
2017-11-06 16:36:24 +0100

    - added measurement function

--- src/u.erl
+++ src/u.erl
@@ -9,13 +9,34 @@
 -module(u).
 -author("Steven").
 
+-define(MEASURE_FILE_NAME, "measurement.log").
+
 %% API
 -compile(export_all).
 
-print(List) -> util:logging("Log", lists:concat([List, "\n"])).
+print(Name, List) -> util:logging(Name, lists:concat([List, "\n"])).
 
-measure(F) ->
-  B = now(),
-  V = F(),
-  A = now(),
-  {timer:now_diff(A,B), V}.
+
+measure(Function, Amount) ->
+  {Best, Worst, All} = measure_(Function, 0, inf, 0, Amount),
+  print(?MEASURE_FILE_NAME,
+    ["Measurements done. Avg: ", All / Amount, ", Best: ", Best, ", Worst: ", Worst]
+  ).
+
+measure_(_, Result, 0) -> Result.
+measure_(Function, Result, Best, Worst, Amount) ->
+  print(?MEASURE_FILE_NAME,
+    ["Measurement number ", util:to_String(Amount), " starting..."]
+  ),
+  {_, Time} = measure(Function),
+  measure_(Function, Result + Time, min(Best, Time), max(Worst, Time), Amount - 1).
+
+measure(Function) ->
+  Start = now(),
+  FunctionResult = Function(),
+  End = now(),
+  Time = timer:now_diff(End, Start),
+  print(?MEASURE_FILE_NAME,
+    ["Measurement result: ", FunctionResult, " after ", Time]
+  ),
+  {FunctionResult, Time}.
